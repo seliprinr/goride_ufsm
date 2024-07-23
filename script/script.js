792431a0c2166d7d7ec2d5f6a7c6304b8d74d72c
Rodrigo Seliprin <rseliprin@gmail.com>
2024-07-22 23:35:01 -0300

    dropdown active
    ativação do menu dropdown

---
+++ script/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const nav = document.querySelector('nav');
+
+    mobileMenu.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
